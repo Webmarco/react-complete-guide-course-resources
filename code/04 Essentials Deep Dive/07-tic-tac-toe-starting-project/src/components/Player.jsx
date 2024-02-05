19136a1367c34be266505c963ddbd974a28e9686
Marco van Dorst <info@vandorst.it>
2024-02-05 12:13:16 +0100

    tictactoe

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick() {
+        setIsEditing((editing) => !editing);
+    }
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+    let buttonCaption = isEditing ? "Save" : "Edit";
+    return (
+        <li>
+            <span className="player">
+                {isEditing ? <input type="text" required value={playerName} onChange={handleChange} /> : <span className="player-name">{playerName}</span>}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
+        </li>
+    );
+}
